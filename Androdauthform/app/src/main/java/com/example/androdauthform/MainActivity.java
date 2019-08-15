9381e15985364c10db6567bf3515fe64e13d509d
artemkoorochka <artem@koorochka.com>
2019-08-15 14:20:02 +0300

    Form request xml for Login

--- Androdauthform/app/src/main/java/com/example/androdauthform/MainActivity.java
+++ Androdauthform/app/src/main/java/com/example/androdauthform/MainActivity.java
@@ -12,12 +12,20 @@ public class MainActivity extends AppCompatActivity {
     private String loginAction = "urn:ICUTech.Intf-IICUTech#Login";
     private String registerAction = "urn:ICUTech.Intf-IICUTech#RegisterNewCustomer";
 
+    // Login0Request
+    String Login0Request="<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+            "<message name=\"Login0Request\">\n" +
+            "<part name=\"UserName\" type=\"xs:string\">artem@koorochka.com</part>\n" +
+            "<part name=\"Password\" type=\"xs:string\"/>artem:android:koorochka</part>\n" +
+            "<part name=\"IPs\" type=\"xs:string\">178.133.140.174</part>\n" +
+            "</message>";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        
+
     }
 }
